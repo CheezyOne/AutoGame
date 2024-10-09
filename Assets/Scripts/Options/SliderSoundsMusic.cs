060129eb993678990796859f880040783d7bba5b
CheezyOne <151920769+CheezyOne@users.noreply.github.com>
2024-10-09 17:39:34 +0300

    Material changing script added

--- Assets/Scripts/Options/SliderSoundsMusic.cs
+++ Assets/Scripts/Options/SliderSoundsMusic.cs
@@ -1,14 +1,63 @@
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(Slider))]
 public class SliderSoundsMusic : MonoBehaviour
 {
+    [SerializeField] private AudioMixer _audioMixer;
+    [SerializeField] private string _mixerVariable;
     [SerializeField] private Material _changingMaterial;
-    private const int FIRSTVALUE = 191, SECONDVALUE = 1;
-    public void ChangeSliderValue(int currentValue)
+    [SerializeField] private float _currentValue, _firstMinIntensity, _firstMaxIntensity, _secondMinIntensity, _secondMaxIntensity, _thirdMinIntensity, _thirdMaxIntensity;
+    private const float FIRST_THRESHOLD = 0.5f;
+    private const float SECOND_THRESHOLD = 0.85f;
+    private Color _firstColor = new Color(191f / 255f, 0f, 0f), _secondColor = new Color(191f / 255f, 191f / 255f, 0f), _thirdColor = new Color(57f/ 255f, 191f / 255f, 0f), _finalColor = new Color(1f / 255f, 191f / 255f, 0f);
+    public void ChangeSliderValue(float currentValue)
     {
-        Color currentColor = _changingMaterial.color;
-        float currentIntensity = (currentColor.r + currentColor.g + currentColor.b) / 3;
-        Color newColor = currentColor * (4 / currentIntensity);
-        _changingMaterial.color = newColor;       
+        _currentValue = currentValue;
+        _audioMixer.SetFloat(_mixerVariable, Mathf.Log10(_currentValue) * 20);
+        ChangeColors();
+    }
+    private void Start()
+    {
+        _audioMixer.GetFloat(_mixerVariable, out _currentValue);
+        _currentValue = Mathf.Pow(10, _currentValue / 20);
+        GetComponent<Slider>().value = _currentValue;
+        ChangeColors();
+    }
+    private void ChangeColors()
+    {
+        Color newColor;
+        float intensityScale;
+
+        if (_currentValue <= FIRST_THRESHOLD)
+        {
+            // Interpolate from red to yellow
+            float t = _currentValue / FIRST_THRESHOLD;
+            newColor = Color.Lerp(_firstColor, _secondColor, t);
+
+            intensityScale = _firstMinIntensity + (_currentValue / FIRST_THRESHOLD) * (_firstMaxIntensity - _firstMinIntensity);
+        }
+        else if (_currentValue <= SECOND_THRESHOLD)
+        {
+            // Interpolate from yellow to green
+            float t = (_currentValue - FIRST_THRESHOLD) / (SECOND_THRESHOLD - FIRST_THRESHOLD);
+            newColor = Color.Lerp(_secondColor, _thirdColor, t);
+            intensityScale = _secondMinIntensity + (_currentValue - FIRST_THRESHOLD) / (SECOND_THRESHOLD - FIRST_THRESHOLD) * (_secondMaxIntensity - _secondMinIntensity);
+        }
+        else
+        {
+            // Interpolate from green to blue
+            float t = (_currentValue - SECOND_THRESHOLD) / (1f - SECOND_THRESHOLD);
+            newColor = Color.Lerp(_thirdColor, _finalColor, t);
+            intensityScale = _thirdMinIntensity + (_currentValue - SECOND_THRESHOLD) / (1f - SECOND_THRESHOLD) * (_thirdMaxIntensity - _thirdMinIntensity);
+        }
+
+        newColor.r *= Mathf.Pow(2, intensityScale);
+        newColor.g *= Mathf.Pow(2, intensityScale);
+        newColor.b *= Mathf.Pow(2, intensityScale);
+
+        // Set color and intensity
+        _changingMaterial.SetVector("_Color", newColor);
     }
 }
